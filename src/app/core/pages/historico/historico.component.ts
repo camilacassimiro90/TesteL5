5f939fc735ba6b615937d6524e61507e24dd31ae
Camila Cassimiro <camilacassimiro@gmail.com>
2023-01-19 14:03:29 -0300

    feat: localStorage

--- src/app/core/pages/historico/historico.component.ts
+++ src/app/core/pages/historico/historico.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+import { RefreshService } from 'src/app/shared/services/refresh.service';
 
 export interface IHistorico {
   inputTexto: string;
   selectTipo: string;
 }
 
-
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.component.html',
   styleUrls: ['./historico.component.css']
 })
+
 export class HistoricoComponent implements OnInit {
   itens: any = [];
 
-  constructor() { }
+  constructor(
+    private storageService: LocalStorageService,
+    private refreshService: RefreshService
+  ) { }
 
   ngOnInit(): void {
+    this.getItens();
+    this.refreshService.getRefresh().subscribe((value: boolean) => {
+      if (value) {
+        this.getItens();
+      }
+    });
   }
 
+  count(event: any) {
+    this.getItens();
+  }
+
+  getItens() {
+    var lista = this.storageService.getAll();
+
+    var filtro = lista.filter((filtro: { inputTexto: any; selectTipo: any; }, index: any, array: any[]) => array.findIndex(f => f.inputTexto == filtro.inputTexto && f.selectTipo == filtro.selectTipo) == index);
+    console.log(filtro);
+    console.log(lista);
+    this.itens = filtro;
+  }
+
+  clear() {
+    this.storageService.clear();
+    this.refreshService.setRefresh(true);
+  }
 }
--- src/app/shared/services/local-storage.service.ts
+++ src/app/shared/services/local-storage.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
 
+export class ListaHistorico {
+  inputTexto!: string;
+  selectTipo!: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class LocalStorageService {
 
-  constructor() { }
+export class LocalStorageService {
+  private storage: Storage;
+
+  constructor() {
+    this.storage = window.localStorage;
+  }
+
+  set(key: string, value: any): boolean {
+    if (this.storage) {
+      this.storage.setItem(key, value);
+      return true;
+    }
+    return false;
+  }
+
+  get(key: string): any {
+    if (this.storage) {
+      return this.storage.getItem(key);
+    }
+    return null;
+  }
+
+  getAll(): any {
+    var listaSalva = [],
+      historico: ListaHistorico[] = [],
+
+      keys = Object.keys(localStorage),
+      i = 0,
+      key;
+    for (; (key = keys[i]); i++) {
+      let item = localStorage.getItem(key);
+      listaSalva.push(item);
+    }
+
+    const map = new Map(Object.entries(listaSalva));
+    map.forEach(function (value, key) {
+      if (typeof value === 'string') {
+        let item = value.split("?");
+        let listaHistorico = new ListaHistorico();
+        listaHistorico.selectTipo = item[0];
+        listaHistorico.inputTexto = item[1];
+        historico.push(listaHistorico);
+      }
+    })
+    return historico;
+  }
+
+  remove(key: string): boolean {
+    if (this.storage) {
+      this.storage.removeItem(key);
+      return true;
+    }
+    return false;
+  }
+
+  clear(): boolean {
+    if (this.storage) {
+      this.storage.clear();
+      return true;
+    }
+    return false;
+  }
 }
